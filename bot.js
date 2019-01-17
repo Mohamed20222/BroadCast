b3e58a5d29b81b45cb9ef30fc3428a7b0b4b0d02
Mohamed20222 <44948619+Mohamed20222@users.noreply.github.com>
2019-01-17 14:43:35 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
 client.on("guildMemberAdd", member => {
     member.createDM().then(function (channel) {
-    return channel.send(`:rose:  ولكم نورت السيرفر:rose: 
+    return channel.send(`:rose: https://discord.gg/SSagmbF:rose: 
   :crown:اسم العضو  ${member}:crown:  
   انت العضو رقم ${member.guild.memberCount} `) 
   }).catch(console.error)
