cc387f8f33ea85709dc9c00c9ae623256db12ff9
Mohamed20222 <44948619+Mohamed20222@users.noreply.github.com>
2019-01-22 10:06:22 +0200

    Delete bot.js

--- bot.js
+++
@@ -1,7 +0,0 @@
-client.on("guildMemberAdd", member => {
-  member.createDM().then(function (channel) {
-  return channel.send(:rose:  ولكم نورت السيرفر:rose: 
-:crown:اسم العضو  ${member}:crown:  
-انت العضو رقم ${member.guild.memberCount}) 
-}).catch(console.error)
-})
